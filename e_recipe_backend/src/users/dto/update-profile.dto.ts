a1e5ac79e2fb53c2e4449b116cefff5c33b4ccbe
lamasiddhu <siddhulama555@gmail.com>
2026-07-30 03:13:00 +0545

    Add Google login, update profile, and notification preference DTOs

---
+++ e_recipe_backend/src/users/dto/update-profile.dto.ts
@@ -0,0 +1,20 @@
+import { IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class UpdateProfileDto {
+  @IsOptional()
+  @IsString()
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  lastName?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(240)
+  bio?: string;
+}
