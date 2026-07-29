88c2ae41c9bd65dabf42564750c9ffac4bd2c70f
lamasiddhu <siddhulama555@gmail.com>
2026-07-30 03:11:53 +0545

    Update auth usecases

--- e_recipe/lib/features/auth/domain/usecases/get_current_usecase.dart
+++ e_recipe/lib/features/auth/domain/usecases/get_current_usecase.dart
@@ -1,14 +1,7 @@
-import 'package:e_recipe/core/error/failures.dart';
+import 'package:e_recipe/core/error/app_result.dart';
 import 'package:e_recipe/core/usecases/app_usecase.dart';
-import 'package:e_recipe/features/auth/data/repositories/auth_repository.dart';
 import 'package:e_recipe/features/auth/domain/entities/auth_entity.dart';
 import 'package:e_recipe/features/auth/domain/repositories/auth_repository.dart';
-import 'package:dartz/dartz.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final getCurrentUseCaseProvider = Provider<GetCurrentUseCase>((ref) {
-  return GetCurrentUseCase(ref.read(authRepositoryProvider));
-});
 
 class GetCurrentUseCase implements UsecaseWithoutParams<AuthEntity> {
   final IAuthRepository _authRepository;
@@ -16,7 +9,7 @@ class GetCurrentUseCase implements UsecaseWithoutParams<AuthEntity> {
   GetCurrentUseCase(this._authRepository);
 
   @override
-  Future<Either<Failure, AuthEntity>> call() {
+  Future<AppResult<AuthEntity>> call() {
     return _authRepository.getCurrentUser();
   }
 }
--- e_recipe/lib/features/auth/domain/usecases/login_usecase.dart
+++ e_recipe/lib/features/auth/domain/usecases/login_usecase.dart
@@ -1,24 +1,13 @@
-import 'package:e_recipe/core/error/failures.dart';
+import 'package:e_recipe/core/error/app_result.dart';
 import 'package:e_recipe/core/usecases/app_usecase.dart';
-import 'package:e_recipe/features/auth/data/repositories/auth_repository.dart';
 import 'package:e_recipe/features/auth/domain/entities/auth_entity.dart';
 import 'package:e_recipe/features/auth/domain/repositories/auth_repository.dart';
-import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final loginUseCaseProvider = Provider<LoginUseCase>((ref) {
-  return LoginUseCase(ref.read(authRepositoryProvider));
-});
-
-class LoginParams extends Equatable {
+class LoginParams {
   final String email;
   final String password;
 
   const LoginParams({required this.email, required this.password});
-
-  @override
-  List<Object?> get props => [email, password];
 }
 
 class LoginUseCase implements UsecaseWithParams<AuthEntity, LoginParams> {
@@ -27,7 +16,7 @@ class LoginUseCase implements UsecaseWithParams<AuthEntity, LoginParams> {
   LoginUseCase(this._authRepository);
 
   @override
-  Future<Either<Failure, AuthEntity>> call(LoginParams params) {
+  Future<AppResult<AuthEntity>> call(LoginParams params) {
     return _authRepository.login(params.email, params.password);
   }
 }
