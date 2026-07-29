18b0b765a95e177562ecddfe16968cdfe58a8e24
lamasiddhu <siddhulama555@gmail.com>
2026-07-30 03:13:03 +0545

    Add recipe DTOs and category constants

---
+++ e_recipe_backend/src/recipes/dto/find-recipes-query.dto.ts
@@ -0,0 +1,40 @@
+import { Type } from 'class-transformer';
+import {
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class FindRecipesQueryDto {
+  @IsOptional()
+  @IsString()
+  category?: string;
+
+  @IsOptional()
+  @IsString()
+  search?: string;
+
+  @IsOptional()
+  @IsIn(['Easy', 'Medium', 'Hard'])
+  difficulty?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  maxTime?: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  limit: number = 10;
+}
